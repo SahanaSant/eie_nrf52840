4f5a6c096ca82a595b57fb3426bf44c6b8533ec3
SahanaSant <sahanasanthanam7@gmail.com>
2025-11-27 20:30:16 -0700

    Finished lesson 8. Onto challenge

--- app/src/main.c
+++ app/src/main.c
@@ -6,11 +6,65 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
+#include <stdio.h>
+#include <string.h>
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/gatt.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/display.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/settings/settings.h>
+#include <zephyr/sys/printk.h>
 
 #include "BTN.h"
 #include "LED.h"
 
+
 #define SLEEP_MS 1
+
+#define BLE_CUSTOM_SERVICE_UUID \
+    BT_UUID_128_ENCODE(0x12345678, 0x1234, 0x5678, 0x1234, 0x56789abcdef0)
+
+#define BLE_CUSTOM_CHARACTERISTIC_UUID \
+    BT_UUID_128_ENCODE(0x12345678, 0x1234, 0x5678, 0x1234, 0x56789abcdef2)
+
+static const struct bt_data ble_advertising_data[] = {
+    // Flags: “I’m BLE only and generally discoverable”
+    BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+
+    // Device name: whatever CONFIG_BT_DEVICE_NAME is set to
+    BT_DATA(BT_DATA_NAME_COMPLETE,
+            CONFIG_BT_DEVICE_NAME,
+            sizeof(CONFIG_BT_DEVICE_NAME) - 1),
+};
+
+static uint8_t ble_custom_characteristic_user_data[20] = {};
+
+static ssize_t ble_custom_characteristic_read_cb(struct bt_conn* conn, const struct bt_gatt_attr* attr,
+                                                 void* buf, uint16_t len, uint16_t offset) {
+  const char* value = attr->user_data;
+  return bt_gatt_attr_read(conn, attr, buf, len, offset, value, strlen(value));
+}
+
+static ssize_t ble_custom_characteristic_write_cb(struct bt_conn* conn, const struct bt_gatt_attr* attr,
+                                                  const void* buf, uint16_t len, uint16_t offset,
+                                                  uint8_t flags) {
+  uint8_t* value_ptr = attr->user_data;
+
+  if (offset + len > BLE_CUSTOM_CHARACTERISTIC_MAX_DATA_LENGTH) {
+    return BT_GATT_ERR(BT_ATT_ERR_INVALID_OFFSET);
+  }
+
+  memcpy(value_ptr + offset, buf, len);
+  value_ptr[offset + len] = 0;
+
+  return len;
+}
 
 int main(void) {
 
